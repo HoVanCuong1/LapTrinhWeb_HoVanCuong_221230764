7bee50fb069c6e2e5be341767bbe3f15fca7c30b
CuongCode <denb11885@gmail.com>
2025-08-28 01:23:13 +0700

    Lab2-B2

---
+++ Controllers/HvcHomeController.cs
@@ -0,0 +1,32 @@
+﻿using Day2_Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Day2_Lab2.Controllers
+{
+    public class HvcHomeController : Controller
+    {
+        private readonly ILogger<HvcHomeController> _logger;
+
+        public HvcHomeController(ILogger<HvcHomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult HvcIndex()
+        {
+            return View();
+        }
+
+        public IActionResult HvcAbout()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day2_Lab2.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult List()
+        {
+            var products = new List<string> { "LapTop", "Phone", "Table" };
+            return View(products);
+        }
+        public IActionResult Detail(string name)
+        {
+            ViewBag.Product = name;
+            return View();
+        }
+
+    }
+}
